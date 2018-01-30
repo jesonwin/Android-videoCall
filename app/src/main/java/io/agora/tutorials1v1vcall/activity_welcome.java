3a16c29a67c71389098c02b877d0511468ea803e
userName <email>
2018-01-30 11:04:44 +0800

    1.基于android的视频通话门禁软件，创建git托管

---
+++ app/src/main/java/io/agora/tutorials1v1vcall/activity_welcome.java
@@ -0,0 +1,25 @@
+package io.agora.tutorials1v1vcall;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class activity_welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        //通过一个时间控制函数Timer，在实现一个活动与另一个活动的跳转。
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(activity_welcome.this,VideoChatViewActivity.class));
+                finish();
+            }
+        }, 4000);//这里停留时间为1000=1s。
+    }
+}
